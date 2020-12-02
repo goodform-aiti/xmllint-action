0984a7525c63dd2b9894a6f229378f1a9a05e1ad
Amir Alian <amircup2006@gmail.com>
2020-12-02 13:56:22 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,4 +8,20 @@ printf ${MODIFIED_FILES}
 
 echo " ******************************"
 
+PATHS=$(printf ${MODIFIED_FILES} | tr \\n '\n')
+XML_FILES=$(grep -P '.*\.xml' <<< $PATHS)
+echo "$XML_FILES" | while read FILE ; do
+    if [[ ! -f $CORE_FILE ]]
+    then
+      # skip deleted files
+      continue
+    fi
+    if xmllint $FILE --noout ; then
+      echo "xml-syntax is fine: $FILE"
+    else
+      echo "syntax error in: $FILE"
+    fi
+done
+
+
 #xmllint $1 --schema $2 --noout
