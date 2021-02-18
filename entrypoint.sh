bb51e8421237090d8b62a876c30e8d3fa088f2f6
Amir Alian <amircup2006@gmail.com>
2021-02-18 11:39:50 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,8 @@ ERROR=0
 
 
 #PATHS=$(printf ${MODIFIED_FILES} | tr \\n '\n')
-XML_FILES=$(find . -type f -name '*.xml' -name 'wsdl.xml' -name 'wsi.xml')
-#XML_FILES=$(grep -P '.+\.xml$' <<< $PATHS)
+PATHS=$(find . -type f -name '*.xml' -name 'wsdl.xml' -name 'wsi.xml')
+XML_FILES=$(grep -P '.+\.xml$' <<< $PATHS)
 
 echo "$XML_FILES" | while read FILE ; do
     if ! xmllint "$FILE" --noout ; then
